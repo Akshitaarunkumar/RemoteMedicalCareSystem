d64498105df02ce9c833c0019a9e7f272fea7473
Akshitaarunkumar <48527299+Akshitaarunkumar@users.noreply.github.com>
2020-10-19 17:16:21 +0530

    Update JavaMailUtil.java

--- Netbeans/JavaMailUtil.java
+++ Netbeans/JavaMailUtil.java
@@ -11,7 +11,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class JavaMailUtil {
-
+//the method to send mail 
     public static void sendMail(String sender, String recipient, String msg, String subject) throws Exception {
         Properties properties = new Properties();
 
