4e1bcee225985d046e333fc62d2af8e1d7243683
Akshitaarunkumar <48527299+Akshitaarunkumar@users.noreply.github.com>
2020-10-19 18:52:43 +0530

    Update JavaMailUtil.java

--- Netbeans/JavaMailUtil.java
+++ Netbeans/JavaMailUtil.java
@@ -11,7 +11,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class JavaMailUtil {
-//the method to send mail 
+    //the method to send mail 
     public static void sendMail(String sender, String recipient, String msg, String subject) throws Exception {
         Properties properties = new Properties();
 
@@ -21,24 +21,29 @@ public class JavaMailUtil {
         properties.put("mail.smtp.port", "587");
 
         String myPass = "";
-
+        //Password authentication
         Session session = Session.getInstance(properties, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(sender, myPass);
             }
         });
-
+        //message preparation to send
         Message message = prepareMessage(session, sender, recipient, msg, subject);
-
+        //to send message
         Transport.send(message);
     }
 
     private static Message prepareMessage(Session session, String myEmail, String recipient, String msg, String subject) {
         try {
+            //session opened to send message
             Message message = new MimeMessage(session);
+            //to set sender mailid
             message.setFrom(new InternetAddress(myEmail));
+            //to set recipient id
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            //to set subject
             message.setSubject(subject);
+            //to set message
             message.setText(msg);
             return message;
         } catch (MessagingException ex) {
